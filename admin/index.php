ed77f2f1a0e248c99b85e81ef0423ecf91cf0d93
Yura Mushii <mushiiyura@gmail.com>
2025-04-02 12:09:04 +0300

    Додавання link_logo

--- admin/index.php
+++ admin/index.php
@@ -28,6 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Farmatek Адмін - Вхід</title>
+  <link rel="shortcut icon" href="img/link_logo.png" type="image/png">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="stylesheet" href="admin-style.css">
 </head>
--- index.php
+++ index.php
@@ -234,6 +234,7 @@ function displayLogo($logo_path, $store_name) {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php echo $store_settings['store_name']; ?> - Акції та Знижки</title>
+<link rel="shortcut icon" href="img/link_logo.png" type="image/png">
 <link rel="stylesheet" href="style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <style>
